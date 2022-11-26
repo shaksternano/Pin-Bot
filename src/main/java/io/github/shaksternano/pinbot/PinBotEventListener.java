0ec2eb1172c340b04118b5817b5dec2eacf0280e
ShaksterNano <54268387+ShaksterNano@users.noreply.github.com>
2022-11-26 20:15:50 +0000

    fix: error message being sent when there's no error

--- src/main/java/io/github/shaksternano/pinbot/PinBotEventListener.java
+++ src/main/java/io/github/shaksternano/pinbot/PinBotEventListener.java
@@ -5,6 +5,7 @@ import club.minnced.discord.webhook.send.WebhookMessageBuilder;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.entities.channel.Channel;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.MessageUpdateEvent;
@@ -60,30 +61,22 @@ public class PinBotEventListener extends ListenerAdapter {
                     .ifPresentOrElse(textChannel -> textChannel.retrieveWebhooks()
                                     .submit()
                                     .thenApply(webhooks -> webhooks.stream()
-                                            .filter(webhook -> PinBotSettings.getWebhook(message.getChannel().getIdLong())
-                                                    .map(webhookId -> webhookId == webhook.getIdLong())
-                                                    .orElse(false)
-                                            ).findAny())
+                                            .filter(PinBotEventListener::isOwnWebhook)
+                                            .findAny()
+                                    )
                                     .thenCompose(webhookOptional -> webhookOptional.map(webhook -> CompletableFuture.supplyAsync(() -> webhook))
-                                            .orElseGet(() -> CompletableFuture.supplyAsync(PinBotEventListener::getSelfIcon)
-                                                    .thenCompose(icon -> textChannel.createWebhook("Pin Bot")
-                                                            .setAvatar(icon)
-                                                            .submit()
-                                                            .thenApply(webhook -> {
-                                                                PinBotSettings.setWebhook(channel.getIdLong(), webhook.getIdLong());
-                                                                return webhook;
-                                                            })
-                                                    )
-                                            )
+                                            .orElseGet(() -> retrieveSelfIcon().thenCompose(icon -> createWebhook(textChannel, icon)))
                                     ).thenAccept(webhook -> transferPinnedMessage(message, webhook))
-                                    .handle((unused, throwable) -> {
-                                        event.getChannel().sendMessage("An error occurred while pinning this message.").queue();
-                                        Main.getLogger().error("An error occurred while pinning a message.", throwable);
-                                        return null;
-                                    }),
-                            () -> event.getChannel().sendMessage("The pin channel for this channel doesn't exist anymore.").queue()
+                                    .handle((unused, throwable) -> handleError(unused, throwable, textChannel)),
+                            () -> event.getChannel().sendMessage("No pin channel set!").queue()
                     );
         }
+    }
+
+    private static boolean isOwnWebhook(Webhook webhook) {
+        return PinBotSettings.getWebhook(webhook.getChannel().getIdLong())
+                .map(webhookId -> webhookId == webhook.getIdLong())
+                .orElse(false);
     }
 
     private static void transferPinnedMessage(Message message, Webhook webhook) {
@@ -117,17 +110,42 @@ public class PinBotEventListener extends ListenerAdapter {
         }
     }
 
-    @Nullable
-    private static Icon getSelfIcon() {
-        String avatarUrl = Main.getJDA().getSelfUser().getAvatarUrl();
-        if (avatarUrl == null) {
-            return null;
+    private static CompletableFuture<Webhook> createWebhook(TextChannel channel, @Nullable Icon icon) {
+        return channel.createWebhook(Main.getJDA().getSelfUser().getName())
+                .setAvatar(icon)
+                .submit()
+                .thenApply(webhook -> {
+                    PinBotSettings.setWebhook(channel.getIdLong(), webhook.getIdLong());
+                    return webhook;
+                });
+    }
+
+    /**
+     * Retrieves the icon of the bot.
+     * The icon will be null if the bot has no avatar.
+     *
+     * @return A {@code CompletableFuture} containing the icon of the bot.
+     */
+    private static CompletableFuture<Icon> retrieveSelfIcon() {
+        return CompletableFuture.supplyAsync(() -> {
+            String avatarUrl = Main.getJDA().getSelfUser().getAvatarUrl();
+            if (avatarUrl == null) {
+                return null;
+            }
+            try {
+                return Icon.from(new URL(avatarUrl).openStream());
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        });
+    }
+
+    private static Void handleError(Void unused, Throwable throwable, MessageChannel channel) {
+        if (throwable != null) {
+            channel.sendMessage("An error occurred while pinning this message.").queue();
+            Main.getLogger().error("An error occurred while pinning a message.", throwable);
         }
-        try {
-            return Icon.from(new URL(avatarUrl).openStream());
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
+        return unused;
     }
 
     public static PinBotEventListener getInstance() {
