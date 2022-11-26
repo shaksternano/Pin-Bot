e45e7848267dd356b58c92ea13df56a16848ccb4
ShaksterNano <54268387+ShaksterNano@users.noreply.github.com>
2022-11-26 20:24:47 +0000

    refactor: move addCommands function to CommandClient

--- src/main/java/io/github/shaksternano/pinbot/CommandClient.java
+++ src/main/java/io/github/shaksternano/pinbot/CommandClient.java
@@ -1,14 +1,60 @@
 package io.github.shaksternano.pinbot;
 
 import io.github.shaksternano.pinbot.command.Command;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class CommandClient {
 
     public static final Map<String, Command> COMMANDS = new HashMap<>();
+
+    public static void addCommands(JDA jda, Command command, Command... commands) {
+        List<Command> commandList = new ArrayList<>(Arrays.asList(commands));
+        commandList.add(command);
+
+        List<? extends CommandData> ungroupedCommands = commandList.stream()
+                .filter(command1 -> command1.getGroup().isEmpty())
+                .map(command1 -> Commands.slash(command1.getName(), command1.getDescription())
+                        .setDefaultPermissions(command1.getPermissions())
+                        .setGuildOnly(command1.isGuildOnly())
+                        .addOptions(command1.getOptions())
+                )
+                .toList();
+        List<? extends CommandData> groupedCommands = commandList.stream()
+                .filter(command1 -> command1.getGroup().isPresent())
+                .collect(Collectors.groupingBy(command1 -> command1.getGroup().orElseThrow()))
+                .entrySet()
+                .stream()
+                .map(entry -> {
+                    String group = entry.getKey();
+                    Collection<Command> commands1 = entry.getValue();
+                    Command firstCommand = commands1.iterator().next();
+                    DefaultMemberPermissions permissions = firstCommand.getPermissions();
+                    boolean guildOnly = firstCommand.isGuildOnly();
+                    List<SubcommandData> subCommands = commands1.stream()
+                            .map(command1 -> new SubcommandData(command1.getName(), command1.getDescription())
+                                    .addOptions(command1.getOptions())
+                            ).toList();
+                    return Commands.slash(group, group)
+                            .setDefaultPermissions(permissions)
+                            .setGuildOnly(guildOnly)
+                            .addSubcommands(subCommands);
+                })
+                .toList();
+
+        jda.updateCommands()
+                .addCommands(ungroupedCommands)
+                .addCommands(groupedCommands)
+                .queue();
+        CommandClient.addCommands(command, commands);
+    }
 
     public static void handleCommand(SlashCommandInteractionEvent event) {
         String commandName = event.getName();
--- src/main/java/io/github/shaksternano/pinbot/Main.java
+++ src/main/java/io/github/shaksternano/pinbot/Main.java
@@ -1,18 +1,16 @@
 package io.github.shaksternano.pinbot;
 
-import io.github.shaksternano.pinbot.command.*;
+import io.github.shaksternano.pinbot.command.PinChannelCommand;
+import io.github.shaksternano.pinbot.command.PinChannelListCommand;
+import io.github.shaksternano.pinbot.command.RemovePinChannelCommand;
+import io.github.shaksternano.pinbot.command.UsesServerProfileCommand;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
-import net.dv8tion.jda.api.interactions.commands.build.CommandData;
-import net.dv8tion.jda.api.interactions.commands.build.Commands;
-import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 public class Main {
 
@@ -31,54 +29,13 @@ public class Main {
                 .enableIntents(GatewayIntent.MESSAGE_CONTENT)
                 .addEventListeners(PinBotEventListener.getInstance())
                 .build();
-        addCommands(
+        CommandClient.addCommands(
                 jda,
                 PinChannelCommand.getInstance(),
                 PinChannelListCommand.getInstance(),
                 RemovePinChannelCommand.getInstance(),
                 UsesServerProfileCommand.getInstance()
         );
-    }
-
-    private static void addCommands(JDA jda, Command command, Command... commands) {
-        List<Command> commandList = new ArrayList<>(Arrays.asList(commands));
-        commandList.add(command);
-
-        List<? extends CommandData> ungroupedCommands = commandList.stream()
-                .filter(command1 -> command1.getGroup().isEmpty())
-                .map(command1 -> Commands.slash(command1.getName(), command1.getDescription())
-                        .setDefaultPermissions(command1.getPermissions())
-                        .setGuildOnly(command1.isGuildOnly())
-                        .addOptions(command1.getOptions())
-                )
-                .toList();
-        List<? extends CommandData> groupedCommands = commandList.stream()
-                .filter(command1 -> command1.getGroup().isPresent())
-                .collect(Collectors.groupingBy(command1 -> command1.getGroup().orElseThrow()))
-                .entrySet()
-                .stream()
-                .map(entry -> {
-                    String group = entry.getKey();
-                    Collection<Command> commands1 = entry.getValue();
-                    Command firstCommand = commands1.iterator().next();
-                    DefaultMemberPermissions permissions = firstCommand.getPermissions();
-                    boolean guildOnly = firstCommand.isGuildOnly();
-                    List<SubcommandData> subCommands = commands1.stream()
-                            .map(command1 -> new SubcommandData(command1.getName(), command1.getDescription())
-                                    .addOptions(command1.getOptions())
-                            ).toList();
-                    return Commands.slash(group, group)
-                            .setDefaultPermissions(permissions)
-                            .setGuildOnly(guildOnly)
-                            .addSubcommands(subCommands);
-                })
-                .toList();
-
-        jda.updateCommands()
-                .addCommands(ungroupedCommands)
-                .addCommands(groupedCommands)
-                .queue();
-        CommandClient.addCommands(command, commands);
     }
 
     public static Logger getLogger() {
