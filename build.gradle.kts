0cfa538a742b4b9ac2e6eea6d66555c3f18ede9c
ShaksterNano <54268387+ShaksterNano@users.noreply.github.com>
2022-12-08 19:24:27 +0000

    build: add shadow gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     id("java")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
-group = "io.github.shaksternano"
-version = "1.0-SNAPSHOT"
+group = project.property("maven_group") as String
+base.archivesName.set(project.property("archives_base_name") as String)
+version = project.property("version") as String
 
 repositories {
     mavenCentral()
@@ -17,6 +21,22 @@ dependencies {
     implementation("com.lmax:disruptor:${project.property("lmax_disruptor_version")}")
     testImplementation("org.junit.jupiter:junit-jupiter-api:${project.property("junit_version")}")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${project.property("junit_version")}")
+}
+
+tasks {
+    named<ShadowJar>("shadowJar") {
+        archiveBaseName.set("${project.property("archives_base_name")}-shadow")
+        mergeServiceFiles()
+        manifest {
+            attributes(mapOf("Main-Class" to "${project.group}.pinbot.Main"))
+        }
+    }
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
 }
 
 tasks.getByName<Test>("test") {
